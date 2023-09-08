972bb26c4baea0d9d2294a67999ed2acda4fa12e
keebtar <144471632+keebtar@users.noreply.github.com>
2023-09-08 18:56:05 -0300

    Add files via upload

--- ausiler.rb
+++ ausiler.rb
@@ -8,7 +8,6 @@ puts "|  ## ## ##    ##   ##  #  ##    ##  ##    ##       ## ##    |"
 puts "| ##      ##   #######   ###     ##  ####  #####    ##  ##   |" 
 puts "--------------------------------------------------------------"
 puts "------------------"
-puts "Devs"
 print "Discord: "
 puts  "keeb.tar"
 print "Criado por: "
@@ -48,7 +47,10 @@ def barra_de_carregamento
   puts "6: mkdir"
   puts "7: ssh"
   puts "8: Desinstalar"
-  puts "9: exit"
+  puts "9: rm (deletar)"
+  puts "10: Criptografia"
+  puts "11: Descriptografar"
+  puts "12: exit"
   print "Escolha: "
     escolha = gets.chomp
 
@@ -63,6 +65,8 @@ def barra_de_carregamento
       barra_de_carregamento
       system("sudo dpkg -i #{arquivo}")
       puts "--------------------------------------------------------------"
+      puts "Pressione enter para continuar."
+      gets.chomp   
       system("ruby ausiler.rb")
     else
 
@@ -75,6 +79,8 @@ def barra_de_carregamento
         barra_de_carregamento
         system("sudo apt-get install #{arquivo} -y")
         puts "--------------------------------------------------------------"
+        puts "Pressione enter para continuar."
+        gets.chomp   
         system("ruby ausiler.rb")
         else
 
@@ -89,6 +95,8 @@ def barra_de_carregamento
           barra_de_carregamento
           system("tar -xf #{arquivo}")
           puts "--------------------------------------------------------------"
+          puts "Pressione enter para continuar."
+          gets.chomp   
           system("ruby ausiler.rb")
         else
 
@@ -103,6 +111,8 @@ def barra_de_carregamento
           barra_de_carregamento
           system("mv #{arquivo}")
           puts "--------------------------------------------------------------"
+          puts "Pressione enter para continuar."
+          gets.chomp   
           system("ruby ausiler.rb")
           else
 
@@ -117,6 +127,8 @@ def barra_de_carregamento
               barra_de_carregamento
               system("cp #{arquivo}")
               puts "--------------------------------------------------------------"
+              puts "Pressione enter para continuar."
+              gets.chomp   
               system("ruby ausiler.rb")
               else
 
@@ -131,6 +143,8 @@ def barra_de_carregamento
               barra_de_carregamento
               system("mkdir #{arquivo}")
               puts "--------------------------------------------------------------"
+              puts "Pressione enter para continuar."
+              gets.chomp   
               system("ruby ausiler.rb")  
             else
             
@@ -142,7 +156,7 @@ def barra_de_carregamento
                 puts "SSH"
                 barra_de_carregamento
                 exec("ssh #{arquivo}")
-                puts "--------------------------------------------------------------"
+                puts "--------------------------------------------------------------"   
               else
 
           if escolha.to_i == 8
@@ -154,17 +168,120 @@ def barra_de_carregamento
             barra_de_carregamento
             system("sudo apt-get remove #{desinstalar} -y")
             puts "--------------------------------------------------------------"
+            puts "Pressione enter para continuar."
+            gets.chomp   
             system("ruby ausiler.rb")
           else
 
             if escolha.to_i == 9
+              system("ls -a")
+              system("pwd")
+              print "Informe o nome do arquivo a ser deletado: "
+              desinstalar = gets.chomp
+              puts "--------------------------------------------------------------"
+              print "Preparando: "
+              puts "Deletando"
+              barra_de_carregamento
+              system("rm -rf #{desinstalar}")
+              puts "--------------------------------------------------------------"
+              puts "Pressione enter para continuar."
+              gets.chomp   
+              system("ruby ausiler.rb")
+            else
+
+          if escolha.to_i == 10
+            def encrypt(text, shift)
+              encrypted_text = ""
+            
+              text.each_char do |char|
+                if char.match(/[a-zA-Z]/)
+                  # Verifica se o caractere é uma letra
+                  shift_amount = shift % 26
+                  shifted_char = char.ord + shift_amount
+            
+                  if char =~ /[A-Z]/ && shifted_char > 'Z'.ord
+                    shifted_char -= 26
+                  elsif char =~ /[a-z]/ && shifted_char > 'z'.ord
+                    shifted_char -= 26
+                  end
+            
+                  encrypted_text << shifted_char.chr
+                else
+                  # Mantém caracteres não alfabéticos inalterados
+                  encrypted_text << char
+                end
+              end
+            
+              return encrypted_text
+            end
+            
+            puts "Digite o texto que deseja criptografar:"
+            plaintext = gets.chomp
+            
+            puts "Digite o valor do deslocamento (um número inteiro positivo ou negativo):"
+            shift = gets.chomp.to_i
+            
+            encrypted_text = encrypt(plaintext, shift)
+            puts "Texto criptografado: #{encrypted_text}"
+            puts "Pressione enter para continuar"
+              gets.chomp
+              system("ruby ausiler.rb")
+          else
+
+            if escolha.to_i  == 11
+              def decrypt(encrypted_text, shift)
+                decrypted_text = ""
+              
+                encrypted_text.each_char do |char|
+                  if char.match(/[a-zA-Z]/)
+                    # Verifica se o caractere é uma letra
+                    shift_amount = shift % 26
+                    shifted_char = char.ord - shift_amount
+              
+                    if char =~ /[A-Z]/ && shifted_char < 'A'.ord
+                      shifted_char += 26
+                    elsif char =~ /[a-z]/ && shifted_char < 'a'.ord
+                      shifted_char += 26
+                    end
+              
+                    decrypted_text << shifted_char.chr
+                  else
+                    # Mantém caracteres não alfabéticos inalterados
+                    decrypted_text << char
+                  end
+                end
+              
+                return decrypted_text
+              end
+              
+              puts "Digite o texto criptografado que deseja descriptografar:"
+              encrypted_text = gets.chomp
+              
+              puts "Digite o valor do deslocamento usado para criptografar (um número inteiro positivo ou negativo):"
+              shift = gets.chomp.to_i
+              
+              decrypted_text = decrypt(encrypted_text, shift)
+              puts "Texto descriptografado: #{decrypted_text}"
+              
+              puts "Pressione enter para continuar"
+              gets.chomp
+              system("ruby ausiler.rb")
+            else
+
+            if escolha.to_i == 12
               puts "Saindo..."
               system("exit")
               else
                 
-                if escolha.to_i <= 9
+                if escolha.to_i <= 12
                 else
+                  puts "Opção invalida, tente novamente."
+                  puts "Pressione enter para continuar."
+                  gets.chomp
                   system("ruby ausiler.rb")
+                      end 
+                     end
+                    end
                    end
                   end
                 end
